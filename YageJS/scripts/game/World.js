2ae9e825009183e14593bb74b2e1cf2b8a09d63b
Charles Covar <covar1@gmail.com>
2012-07-07 10:08:12 -0400

    Cleans up YageJS and World modules.

--- YageJS/scripts/game/World.js
+++ YageJS/scripts/game/World.js
@@ -1,16 +1,17 @@
-define([], function () {
+define(['render/ArtAsset2d'], function (ArtAsset2d) {
 	/*
 	 * Defines the size of the current level, worlds operate on the first
 	 * quadrant. The camera module will handle the translation to quadrant four
 	 * Width and Height of the world must be at least the same as the Screen.
 	 */
-	function World(width, height) {
-		var w = width,
-			h = height;
-		
-		this.getWidth = function () { return w; };
-		this.getHeight = function () { return h; };
+	function World(id) {
+		var that = new ArtAsset2d(id);
+
+		that.getWidth = function () { return this.w[0]; };
+		that.getHeight = function () { return this.h[0]; };
+
+		return that;
 	}
-	
+
 	return World;
 });
--- YageJS/scripts/game/YageJS.js
+++ YageJS/scripts/game/YageJS.js
@@ -1,35 +1,25 @@
-define(['game/Player', 'utils/keyCodes', 'game/EntityFactory', 'render/Screen'], function (Player, keys, entityFactory, page) {
+define(['game/Player',
+        'game/World',
+        'utils/keyCodes',
+        'game/EntityFactory',
+        'render/Camera',
+        'render/Screen'], function (Player, World, keys, entityFactory, Camera, page) {
 	var Game = {},
-		VELOCITY = 8,
-		Players = [];
+		player = new Player('sprite'),
+		world = new World('testWorld'),
+		camera = new Camera(player, world);
 
-	Game.keys = keys;
-	Game.VELOCITY = VELOCITY;
-	Game.Players = Players;
 	Game.entityFactory = entityFactory;
 
-	Players.push(new Player('sprite'));
-
 	Game.update = function () {
-		var i = 0;
-
-		for (i = 0; i < Players.length; i += 1) {
-			Players[i].update();
-		}
+		player.update();
+		camera.update();
 
 		entityFactory.update();
 	};
 
 	Game.draw = function () {
-		var i = 0;
-
-		page.clear();
-
-		for (i = 0; i < Players.length; i += 1) {
-			Players[i].draw();
-		}
-
-		entityFactory.draw();
+		camera.draw();
 	};
 
 	return Game;
