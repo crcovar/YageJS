775dc7986088829f4bf6d2de0acb18130d757bc9
Charles Covar <covar1@gmail.com>
2012-07-13 15:41:42 -0400

    Uses setInterval() to reduce complexity.
    main.js now uses setInterval() instead of setTimeout(). This reduces the
    complexity as the call only needs to be made once for the life of the
    engine rather than on each iteration of the loops.

--- YageJS/scripts/main.js
+++ YageJS/scripts/main.js
@@ -9,18 +9,7 @@ require(['game/YageJS', 'utils/systemEvents!'], function(Game) {
 	var RATE30 = 1000 / 30,
 		RATE60 = 1000 / 60;
 
-	function update(rate) {
-		Game.update();
-		setTimeout(function () { return update(rate); }, rate);
-	}
-
-	function draw(rate) {
-		Game.draw();
-		setTimeout(function () { return draw(rate); }, rate);
-	}
-
 	// start running the loops
-	update(RATE30);
-	draw(RATE30);
-
+	setInterval(Game.update, RATE30);
+	setInterval(Game.draw, RATE30);
 });
