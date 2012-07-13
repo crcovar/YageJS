455d70c8f94af39bd01ea1a3515d175311da750b
Charles Covar <covar1@gmail.com>
2012-07-13 10:00:39 -0400

    Camera receives draw events.
    Camera module now recieves draw events, converts world coordinates to
    view coordinates and tells the ArtAssetFactory to render the image.

--- YageJS/scripts/render/ArtAsset2d.js
+++ YageJS/scripts/render/ArtAsset2d.js
@@ -1,4 +1,4 @@
-﻿define(function () {
+﻿define(['render/Screen'], function (screen) {
 	function ArtAsset2d(id) {
 		var request = new XMLHttpRequest(),
 			that = {};
@@ -10,6 +10,44 @@
 		that.image = new Image();
 		that.image.src = 'assets/' + id + '/' + id + '.png';
 
+		that.state = 'default';
+		that.currentFrame = 0;
+
+		that.changeState = function (state) {
+			if(this.state !== state) {
+				this.state = state;
+				this.currentFrame = 0;
+			}
+		};
+
+		that.getSize = function () {
+			return {
+				w: this.w[this[this.state].animation[this.currentFrame]],
+				h: this.h[this[this.state].animation[this.currentFrame]]
+			};
+		};
+
+		/**
+		 * draws the asset to the screen, update the frame
+		 * @param x view coordinate
+		 * @param y view coordinate
+		 */
+		that.draw = function (x, y) {
+			screen.drawImage(this.image,
+					this.x[this[this.state].animation[this.currentFrame]],
+					this.y[this[this.state].animation[this.currentFrame]],
+					this.w[this[this.state].animation[this.currentFrame]],
+					this.h[this[this.state].animation[this.currentFrame]],
+					x, y,
+					this.w[this[this.state].animation[this.currentFrame]],
+					this.h[this[this.state].animation[this.currentFrame]]);
+
+			if (this.currentFrame < this[this.state].animation.length - 1) {
+				this.currentFrame += 1;
+			} else if (this[this.state].repeat) {
+				this.currentFrame = 0;
+			}
+		};
 
 		return that;
 	}
--- YageJS/scripts/render/Camera.js
+++ YageJS/scripts/render/Camera.js
@@ -1,22 +1,35 @@
-define(['render/Screen'], function (Screen) {
-	var width = Screen.getWidth(),
-		height = Screen.getHeight();
+define(['render/ArtAssetFactory', 'render/Screen', 'utils/EventHandler'], function (artAssetFactory, screen, eventHandler) {
+	var width = screen.getWidth(),
+		height = screen.getHeight();
+
+	function Drawable(x, y, image) {
+		this.x = x;
+		this.y = y;
+		this.image = image;
+	}
 
 	function Camera(/*Entity*/entity, /*World*/world) {
+		var self = this;
+
 		this._subject = entity;
 		this._world = world;
-	
+		this._toDraw = [];
+		this._x = 0;
+		this._y = 0;
+
 		/* used for scrolling, these are percent values (float) from the edges
 		of the camera to start scrolling. If these are null the camera will
 		never scroll on that axis. */
 		this._horizontalBound = null;
 		this._verticalBound = null;
+
+		eventHandler.on('draw', function (event) { return self.onDraw(event); });
 	}
-	
+
 	Camera.prototype.changeWorld = function (/*World*/world) {
 		this._world = world;
 	};
-	
+
 	Camera.prototype.update = function () {
 		this._x = this._subject.x;
 		this._y = this._subject.y;
@@ -34,6 +47,22 @@ define(['render/Screen'], function (Screen) {
 			this_y = this._world.getHeight() - height / 2;
 		}
 	};
-	
+
+	Camera.prototype.onDraw = function (event) {
+		var x = event.x - (this._x - (width / 2)),
+			y = height - (event.y - (this._y - (height / 2)));
+		artAssetFactory.draw(x, y, event.reference);
+	};
+
+	Camera.prototype.draw = function () {
+		screen.clear();
+
+		screen.drawImage(this._world.image, this._x - (width / 2),
+				this._world.getHeight() - this._y - (height / 2), width, height,
+				0, 0, width, height);
+
+		this._subject.draw();
+	};
+
 	return Camera;
 });
