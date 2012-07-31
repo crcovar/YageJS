dcb243aaf4d9e3467396b227f7c52ef4214a0912
Charles Covar <covar1@gmail.com>
2012-07-31 14:17:22 -0400

    Refactors host.js

--- Servers/host.js
+++ Servers/host.js
@@ -1,75 +1,97 @@
 var http = require("http"),
     url = require("url"),
     fs = require("fs");
+
+/* Functions for serving the different content */
+function send404(res) {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end(fs.readFileSync('Servers/404.html', 'utf8'));
+}
     
+function serveHtml(path, res) {
+    var file;
+    try {
+        file = fs.readFileSync(path);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(file, 'utf8');
+    } catch (err) {
+        send404(res);
+    } 
+}
+    
+function serveJavascript(path, res) {
+    var file;
+    try {
+        file = fs.readFileSync(path, 'utf8');
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+        res.end(file);
+    } catch (err) {
+       send404(res);
+    }
+}
+
+function serveJson(path, res) {
+    var file;
+    try {
+        file = fs.readFileSync(path, 'utf8');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(file);
+    } catch (err) {
+        send404(res);
+    }
+}
+
+function servePng(path, res) {
+    var file;
+    try {
+        file = fs.readFileSync(path);
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end(file);
+    } catch (err) {
+        send404(res);
+    }
+}
+
+function serveAudio(path, res) {
+    send404(res);
+}
+
+/* Setup environment settings if needed */
+if (!process.env.IP) {
+    process.env.IP = '127.0.0.1';
+}
+if (!process.env.PORT) {
+    process.env.PORT = '80';
+}
+
+/* Create the hosting server */
 http.createServer(function (req, res) {
     var pathname = url.parse(req.url).pathname,
-        path = pathname.substr(1),
-        file,
-        error = false;
+        path = pathname.substr(1);
+        
+    if (path === '') {
+        send404(res);
+    }
         
     if (/\/$/.test(path)) {
-        try {
-            file = fs.readFileSync(path + 'index.html');
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(file, 'utf8');
-        } catch (err) {
-            error = true;
-        }            
+        serveHtml(path + 'index.html', res);
     }
     
     if (/\.(?:js)$/.test(path)) {
-        try {
-            file = fs.readFileSync(path, 'utf8');
-            res.writeHead(200, {'Content-Type': 'application/javascript'});
-            res.end(file);
-        } catch (err) {
-            error = true;
-        }
+        serveJavascript(path, res);
     }
     
     if (/\.(?:json)$/.test(path)) {
-        try {
-            file = fs.readFileSync(path, 'utf8');
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(file);
-        } catch (err) {
-            error = true;
-        }
+        serveJson(path, res);
     }
     
     if (/\.(?:png)$/.test(path)) {
-        try {
-            file = fs.readFileSync(path, 'binary');
-            res.writeHead(200, {'Content-Type': 'image/png'});
-            res.end(file);
-        } catch (err) {
-            error = true;
-        }
+        servePng(path, res);
     }
     
-    if (/\.(?:mp3)$/.test(path)) {
-        try {
-            file = fs.readFileSync(path, 'binary');
-            res.writeHead(200, {'Content-Type': 'audio/mpeg3'});
-            res.end(file);
-        } catch (err) {
-            error = true;
-        }
+    if (/\.(?:mp3)$/.test(path) || /\.(?:wav)$/.test(path)) {
+        serveAudio(path, res);
     }
-    if (/\.(?:wav)$/.test(path)) {
-        try {
-            file = fs.readFileSync(path, 'binary');
-            res.writeHead(200, {'Content-Type': 'audio/wav'});
-            res.end(file);
-        } catch (err) {
-            error = true;
-        }
-    }
-    
-    if (error) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end(fs.readFileSync('Server/404.html', 'utf8'));
-    }
+
 }).listen(process.env.PORT, process.env.IP);
 console.log('server running on ' + process.env.IP + ':' + process.env.PORT);
