f725202c983f890b4065443482347b0916fcce61
Charles Covar <covar1@gmail.com>
2012-08-03 16:45:16 -0400

    Uses Connect module for serving up content.
    The included server now uses the node.js connect module to serve up
    content. This fixes the bug preventing audio from streaming, and
    drastically cuts down on the amount of code that needs to be maintained.

--- Servers/host.js
+++ Servers/host.js
@@ -1,34 +1,4 @@
-var http = require("http"),
-    url = require("url"),
-    fs = require("fs");
-
-/**
- * Reads a url path and serves up the appropriate file.
- * @param path Relative location to the project root of the object to be served.
- * @param res The HTTP(S) Response object
- */
-function send(path, res) {
-    var stat,
-        type;
-    
-    if (path.substr(-1) === '/') { type = 'text/html'; path += 'index.html'; }
-    if (path.substr(-3) === '.js') { type = 'application/javascript'; }
-    if (path.substr(-5) === '.json') { type = 'application/json'; }
-    if (path.substr(-4) === '.png') { type = 'image/png'; }
-    if (path.substr(-4) === '.mp3') { type = 'audio/mpeg'; }
-
-    try {
-        stat = fs.statSync(path);
-        res.writeHead(200, {
-            'Content-Type': type,
-            'Content-Length': stat.size
-        });
-        fs.createReadStream(path).pipe(res);
-    } catch (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream('Servers/404.html').pipe(res);
-    }
-}
+var connect = require('connect');
 
 /* Setup environment settings if needed */
 if (!process.env.IP) {
@@ -38,11 +8,9 @@ if (!process.env.PORT) {
     process.env.PORT = '80';
 }
 
-/* Create the hosting server */
-http.createServer(function (req, res) {
-    var pathname = url.parse(req.url).pathname,
-        path = pathname.substr(1);
-
-    send(path, res);
-}).listen(process.env.PORT, process.env.IP);
+connect()
+    .use(connect.logger())
+    .use(connect.compress())
+    .use(connect.static(__dirname + '/..'))
+    .listen(process.env.PORT, process.env.IP);
 console.log('server running on ' + process.env.IP + ':' + process.env.PORT);
