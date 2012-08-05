cafdf5f92f7c401a6ace3ac36e72f02f7c081b9a
Charles Covar <covar1@gmail.com>
2012-08-04 21:49:56 -0400

    Removes hostname from listen()

--- Servers/host.js
+++ Servers/host.js
@@ -1,16 +1,12 @@
 var connect = require('connect');
 
 /* Setup environment settings if needed */
-if (!process.env.IP) {
-    process.env.IP = '127.0.0.1';
-}
 if (!process.env.PORT) {
     process.env.PORT = '80';
 }
 
-connect()
-    .use(connect.logger())
+connect().use(connect.logger())
     .use(connect.compress())
     .use(connect.static(__dirname + '/..'))
-    .listen(process.env.PORT, process.env.IP);
-console.log('server running on ' + process.env.IP + ':' + process.env.PORT);
+    .listen(process.env.PORT);
+console.log('server running on port ' + process.env.PORT);
